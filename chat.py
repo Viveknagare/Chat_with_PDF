367f5821e9a8f13889ddf561580407948efd5ac8
Viveknagare <vivekbnagare31@gmail.com>
2026-04-01 22:53:11 +0530

    Added the chat.py file in which wrote the code for retrival of Data from the Model which actually finds important chunks based on user query from vector embeddings which are stored in vector database(Qdrant DB)

---
+++ chat.py
@@ -0,0 +1,45 @@
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+openai_client = OpenAI()
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-large"
+)
+
+vector_db = QdrantVectorStore.from_existing_collection(
+    embedding=embeddings,
+    url="http://localhost:6333",
+    collection_name="Learning_RAG"
+)
+
+user_query = input("Ask Something: ")
+
+#relevant chunks from VectorDB
+search_result = vector_db.similarity_search(user_query)
+
+context = "\n\n\n".join([f"Page Content: {result.page_content}\n Page Number: {result.metadata['page_label']} \nFile Location: {result.metadata['source']}" for result in search_result
+])
+
+SYSTEM_PROMPT = f"""
+You are Helpful AI Assistant who answers user query based on available context retrived from PDF file along with page_contents and page number.
+
+you should only answer the user based on the following context and navigate the user to open the right page number to know more
+
+Context:
+{context}
+"""
+
+response = openai_client.chat.completions.create(
+    model="gpt-5",
+    messages=[
+        {"role" : "system", "content" : SYSTEM_PROMPT},
+        {"role" : "user", "content" : user_query}
+    ]
+)
+
+print(f"🤖 {response.choices[0].message.content}")
