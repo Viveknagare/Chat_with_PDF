ff79c0bc19e7e8f28813c30e7b299d45fd96c43e
Viveknagare <vivekbnagare31@gmail.com>
2026-04-05 00:34:00 +0530

    The before code written is able to handle one user query, so inorder to make the server handle multiple user queries, I have integrated Python RQ which is python redis queue,and for the redis environment I have used Valkey which is similar to redis, nowthe user query will be queued in the queue in rq_client file , this will happen when user will make post request in which his query will be present , which will be sent in the job Queue, till now, I havent wrote the processor server, now the user will get only his status as queued and his job id as status

---
+++ rag_queue/queues/worker.py
@@ -0,0 +1,48 @@
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
+from openai import OpenAI
+
+openai_client = OpenAI()
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-large"
+)
+
+vector_db = QdrantVectorStore.from_existing_collection(
+    embedding=embeddings,
+    url="http://localhost:6333",
+    collection_name="Learning_RAG"
+)
+
+def process_query(query:str):
+    print("Searching chunks", query)
+    search_result = vector_db.similarity_search(query)
+
+    context = "\n\n\n".join([f"Page Content: {result.page_content}\n Page Number: {result.metadata['page_label']} \nFile Location: {result.metadata['source']}" for result in search_result
+    ])
+
+    SYSTEM_PROMPT = f"""
+    You are Helpful AI Assistant who answers user query based on available      context retrived from PDF file along with page_contents and page number.
+    
+    you should only answer the user based on the following context and navigate      the user to open the right page number to know more
+
+    Context:
+    {context}
+    """
+
+    response = openai_client.chat.completions.create(
+       model="gpt-5",
+       messages=[
+           {"role" : "system", "content" : SYSTEM_PROMPT},
+           {"role" : "user", "content" : query}
+       ]
+    )
+
+    print(f"🤖: {response.choices[0].message.content}")
+    return response.choices[0].message.content
+        
+
+
+
+
